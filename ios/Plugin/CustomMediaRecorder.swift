dfef15f4379b233b6e89ebeeaba4bca7ac8c8c7f
Nemanja <hello@modular-web.com>
2024-05-12 03:08:14 +0200

    Update 6.0

--- ios/Plugin/CustomMediaRecorder.swift
+++ ios/Plugin/CustomMediaRecorder.swift
@@ -10,9 +10,12 @@ class CustomMediaRecorder {
     private var status = CurrentRecordingStatus.NONE
     
     private let settings = [
-        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+        AVFormatIDKey: Int(kAudioFormatLinearPCM),
         AVSampleRateKey: 44100,
         AVNumberOfChannelsKey: 1,
+        AVLinearPCMBitDepthKey: 16,
+        AVLinearPCMIsBigEndianKey: false,
+        AVLinearPCMIsFloatKey: false
         AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
     ]
     
@@ -26,7 +29,7 @@ class CustomMediaRecorder {
             originalRecordingSessionCategory = recordingSession.category
             try recordingSession.setCategory(AVAudioSession.Category.playAndRecord)
             try recordingSession.setActive(true)
-            audioFilePath = getDirectoryToSaveAudioFile().appendingPathComponent("\(UUID().uuidString).aac")
+            audioFilePath = getDirectoryToSaveAudioFile().appendingPathComponent("\(UUID().uuidString).wav")
             audioRecorder = try AVAudioRecorder(url: audioFilePath, settings: settings)
             audioRecorder.record()
             status = CurrentRecordingStatus.RECORDING
